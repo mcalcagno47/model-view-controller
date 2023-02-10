90e09a0d6b60f90e530a57598749c4dcca3822b4
Mark Calcagno <mcalcagn@gmail.com>
2023-02-09 21:47:38 -0600

    fixing routes

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -57,3 +57,24 @@ document
 document
     .querySelector('#delete-btn')
     .addEventListener('click', deleteClickHandler);
+
+
+//     Ok so this error- is the postId that is defined at the top (which it looks like you are no longer using) I would get rid of that top line and don't forget to replace that variable postId with the id you are actually using from line 10 that you are grabbing from the window
+// That will go into the put route instead of /postId
+// **Also - check to make sure your route matches up exactly - hint (/post vs /posts) for the api route you are trying to hit
+// Also double check how you are grabbing your text values --> and if you get errors from those make sure to check that the id/input form matches on the edit-post handlebars page
+
+// So to summarize
+// 1. Delete postID and replace it in route
+// 2. Check route to make sure correct
+// 3. Check values are getting grabbed (could add console log to check they are showing up in browser terminal)  *input vs text are*a
+// white_check_mark
+// eyes
+// raised_hands
+
+
+
+
+
+// 9:10
+// But all work should be done within the edit-post.js page to get this guy working! You are closer than you think!
